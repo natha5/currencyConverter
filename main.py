7145045849f5f8990f0d2164db252246741251fc
natha5 <ns539@canterbury.ac.uk>
2022-06-04 18:37:26 +0100

    <mod> finished basic ui

--- main.py
+++ main.py
@@ -13,14 +13,24 @@ class Application(Frame):
         ]
         self.option_var = tk.StringVar(self)
 
-        self.createWidgets()
+        self.create_widgets()
 
-    def createWidgets(self):
-        # label for initial amount box
+    def create_widgets(self):
+
+
+        # label for input currency drop down
         self.lbl_initCurrency = Label(self, text="Initial currency")
-        self.lbl_initCurrency.grid(row=0, column=0, columnspan=2, sticky=W)
+        self.lbl_initCurrency.grid(row=0, column=0, columnspan=1, sticky=W)
 
-        # drop down to select currency
+        # label for input amount entry
+        self.lbl_initAmount = Label(self, text="Input amount")
+        self.lbl_initAmount.grid(row=0, column=1, columnspan=1, sticky=W)
+
+        # label for output currency drop down
+        self.lbl_outputCurrency = Label(self, text="Output currency")
+        self.lbl_outputCurrency.grid(row=0, column=3, columnspan=1, sticky=W)
+
+        # drop down to select input currency
         self.opm_inputCurrency = OptionMenu(self, self.option_var, self.currencyOptions, *self.currencyOptions)
         self.opm_inputCurrency.grid(row=1, column=0, sticky=W)
 
@@ -28,22 +38,27 @@ class Application(Frame):
         self.ent_initAmount = Entry(self)
         self.ent_initAmount.grid(row=1, column=1, sticky=W)
 
-        self.bttn_convert = Button(self, text='Submit')
-        self.bttn_convert.grid(row=1, column=2, sticky=W)
-
+        # equals sign to separate input and output visually
         self.lbl_equals = Label(self, text = "=")
-        self.lbl_equals.grid(row=1, column = 3, columnspan= 3, sticky = W)
+        self.lbl_equals.grid(row=1, column = 2, columnspan= 3, sticky = W)
 
-        # drop down to select currency
+        # drop down to select output currency
         self.opm_outputCurrency = OptionMenu(self, self.option_var, self.currencyOptions, *self.currencyOptions)
-        self.opm_outputCurrency.grid(row=1, column=4, sticky=W)
+        self.opm_outputCurrency.grid(row=1, column=3, sticky=W)
+
+        # result label
+        self.lbl_outputAmount = Label(self, text = "result")
+        self.lbl_outputAmount.grid(row = 1, column = 4, sticky = W)
+
+        self.bttn_convert = Button(self, text='Submit')
+        self.bttn_convert.grid(row=2, column=0, sticky=W)
 
 
 root = Tk()
 
 root.title("Currency Converter")
 
-root.geometry("300x150")
+root.geometry("500x250")
 
 app = Application(root)
 
